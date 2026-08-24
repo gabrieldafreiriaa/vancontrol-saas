578b9dc082428d89757e6991a57542d05bc41264
gabrieldafreiriaa <gabrieldafreiriaa@gmail.com>
2026-08-24 14:07:05 -0300

    feat:primeira estrutura do front

---
+++ client/src/pages/alunos/Alunos.jsx
@@ -0,0 +1,11 @@
+function Alunos() {
+  return (
+    <div>
+      <h1>VanControl</h1>
+      <h2>Tela de Alunos</h2>
+      <p>Frontend iniciado com sucesso.</p>
+    </div>
+  );
+}
+
+export default Alunos;
---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,7 @@
+import Alunos from '../pages/alunos/Alunos.jsx';
+
+function AppRoutes() {
+  return <Alunos />;
+}
+
+export default AppRoutes;
